ad97242fb1c3930c1c0af9855d127a0385042cb9
dhavalmanjaria <Dhaval Anjaria>
2022-02-23 15:31:39 +0530

    Add and fix frontend exception handler

--- decorators/handle_generic_exception.py
+++ decorators/handle_generic_exception.py
@@ -3,15 +3,32 @@ from functools import wraps
 from config import default_log
 from data.db.init_db import get_db
 from standard_responses.dbapi_exception_response import DBApiExceptionResponse
+from fastapi import HTTPException
 
 
-
+ 
+def frontend_api_generic_exception(function):
+    @wraps(function)
+    def decorated_function(*args, **kwargs):
+        try:
+            return function(*args, **kwargs)
+        except Exception as e:
+            default_log.exception(e)
+            #TODO: Use JSONResponse
+            raise HTTPException(
+                status_code=400,
+                detail=str(e),
+            )
+    return decorated_function
+ 
+ 
 def dbapi_exception_handler(function):
     @wraps(function)
     def decorated_function(*args, **kwargs):
-        db = next(get_db())
+        db = None
         try:
-            kwargs['session'] = db
+            if not kwargs.get('session'):
+                kwargs['session'] = next(get_db())
             retval = function(*args, **kwargs)
             if kwargs.get('commit'):
                 db.close()
@@ -21,5 +38,6 @@ def dbapi_exception_handler(function):
             default_log.exception(e)
             return DBApiExceptionResponse(error=str(e))
         finally:
-            db.close()
+            if db is not None:
+                db.close()
     return decorated_function
